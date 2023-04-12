92a3470fd65cd3047ae002b3a61251311c88cd73
최승호 <dreamdp01@sungkyul.ac.kr>
2023-04-12 05:18:44 +0000

    자바 웹프 6주차

---
+++ js/data_type.js
@@ -0,0 +1,8 @@
+let number = 5;
+let str ='문자열 입력';
+let prime =1.5123;
+let is_ok =true;
+let is_not=false;
+let undefi;
+let empty;
+console.log(undefi,empty);
---
+++ js/hover.js
@@ -0,0 +1,6 @@
+function over(obj){
+	obj.src = "img/google_logo_4.png";
+}
+function out(obj){
+	obj.src = "img/google_logo_3.png";
+}
